19946181dd50eb0503cc42961221d02274473bf5
Alexis AMTMANN <amtman25al@lycee-ndduroc.com>
2024-04-10 21:10:39 +0200

    Initial commit

---
+++ Flocks-Desk/Models/Api.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators.OAuth2;
+
+namespace Flocks_Desk.Models
+{
+    internal class Api
+    {
+        private static String _url = Properties.Resources.API_URL;
+        private static String _version = Properties.Resources.API_VERSION;
+        public static String Token { get; set; }
+        public static JsonSerializerOptions jsonOptions = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true };
+
+        public static RestResponse Post(String route, Dictionary<String, String> fields = null)
+        {
+            // HTTP instance object where to send the request
+            RestClient clientApi = new RestClient(Api._url + Api._version);
+
+            // Prepare request
+
+            RestRequest request = new RestRequest(route, Method.Post);
+            request.Resource = route;
+            request.AddJsonBody(fields);
+
+            // send HTTP request and return response
+            return clientApi.Execute(request);
+
+        }
+        public static RestResponse GetWithToken(String route, Dictionary<String, String> fields = null)
+        {
+#if DEBUG
+            Console.WriteLine("Api - GetWithToken");
+#endif
+            OAuth2AuthorizationRequestHeaderAuthenticator authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(Token, "Bearer");
+
+            RestClientOptions options = new RestClientOptions(Api._url + Api._version)
+            {
+                Authenticator = authenticator
+            };
+            RestClient clientApi = new RestClient(options);
+
+            //Prepare request
+            RestRequest request = new RestRequest(route, Method.Get);
+
+            // Just include necessary field (otherwise request will fail)
+            if (null != fields)
+            {
+                foreach (KeyValuePair<String, String> field in fields)
+                {
+                    request.AddUrlSegment(field.Key, field.Value);
+                }
+
+            }
+
+            // send HTTP request and return response
+            return clientApi.Execute(request);
+
+
+        }
+    }
+}
---
+++ Flocks-Desk/Models/Auth.cs
@@ -0,0 +1,61 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Flocks_Desk.Models
+{
+    internal class Auth
+    {
+
+        // Sub class inside Auth class. Used to acces token according to the JSON responds to /login route.
+        public class SuccessMessage
+        {
+            public string Token { get; set; }
+        }
+
+        // SuccessMessage Property (as defined by the above class).
+        public SuccessMessage Success { get; set; }
+
+        public static String Login(String email, String pwd)
+        {
+            // associative array with email and password
+            Dictionary<String, String> userCredentials = new Dictionary<string, string>();
+            userCredentials.Add("email", email);
+            userCredentials.Add("password", pwd);
+
+            // call /login route to API
+            RestResponse response = Api.Post("login", userCredentials);
+            Console.WriteLine(response.Content);
+
+            // Deserialization du Json en une instance de la classe login
+            Auth result = JsonSerializer.Deserialize<Auth>(response.Content, Api.jsonOptions);
+
+            // Print response in console
+            Console.WriteLine(response.Content);
+
+            // Assign message according to response status code
+            string message = "";
+            switch (response.StatusCode)
+            {
+                case System.Net.HttpStatusCode.OK:
+                    message = "Success";
+                    Api.Token = result.Success.Token;
+                    Console.WriteLine(result.Success.Token);
+                    break;
+                case System.Net.HttpStatusCode.Unauthorized:
+                    message = "Unauthorized access. Please try again !";
+                    break;
+                default:
+                    message = "Something wrong occured with the connection to database";
+                    break;
+            }
+            return message;
+
+            //   return null;
+        }
+    }
+}
